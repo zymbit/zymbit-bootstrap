c0a4d2514346ed19558f7f13aafac16ed54088ff
Roberto Aguilar <roberto.c.aguilar@gmail.com>
2015-04-28 11:02:58 -0400

    Added initial zymbit bootstrap for raspbian

---
+++ zymbit-bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+[ "$USER" != "root" ] && echo "run this as root" && exit 1
+
+#apt-get update
+
+# install system packages
+apt-get install -y collectd daemontools-run python-dev python-setuptools
+
+
+# install python tools
+pip_path=`which pip`
+if [ "$pip_path" = "" ]; then
+    easy_install pip
+    pip install virtualenv
+fi;
+
+
+# install zymbit in a virtualenv
+mkdir -p /opt/zymbit
+
+VENV=/opt/zymbit/venv
+[ ! -e "$VENV" ] && virtualenv $VENV
+
+LATEST=/opt/zymbit/latest
+[ ! -e "$LATEST" ] && ln -s $VENV $LATEST
+
+/opt/zymbit/latest/bin/pip install -U zymbit
+
+echo "You're ready to provision this device.  Please copy your provisioning token from the console at https://zymbit.com/console"
+echo "Then run the command:\n/opt/zymbit/latest/bin/zymbit provision <provision token>"
+
+exit 0
